9c7f1d0c70605ad4f995289f1063d44a09f88f1c
gedekay <gedekay@gmail.com>
2017-12-17 18:44:23 -0500

    Finished day 10

--- src/advent_of_code/day10-2017.clj
+++ src/advent_of_code/day10-2017.clj
@@ -3,16 +3,18 @@
   (:gen-class))
 
 (def input-file "resources/day10-2017.txt")
-
 (def in-list (into [] (range 256)))
-
+(def default-end [17 31 73 47 23])
 (def start-pos 0)
-
 (def start-skip-size 0)
+(def partition-size 16)
+(def rounds 64)
 
-(def lengths (map #(Integer/parseInt %) (string/split (slurp input-file) #"[,\n]")))
+(def lengths-1 (map #(Integer/parseInt %) (string/split (slurp input-file) #"[,\n]")))
 
 (defrecord KnotState [list position skip-size])
+
+(def start-knot-state (KnotState. in-list start-pos start-skip-size))
 
 (defn make-move
   [k-state length]
@@ -22,6 +24,73 @@
          list-length (count start-state)
          next-index (+ start-position length)
          idx-end (mod next-index list-length)
-         
-         selection (subvec )]
+         circled? (= 1 (quot next-index list-length))
+         selection (if circled? 
+                     (into [] 
+                           (concat (subvec start-state start-position list-length) 
+                                   (subvec start-state 0 idx-end)))
+                     (subvec start-state start-position idx-end))
+         rev-selection (into [] (reverse selection))
+         new-state (if circled?
+                     (into [] 
+                           (concat (drop (- list-length start-position) rev-selection) 
+                                   (subvec start-state idx-end start-position) 
+                                   (take (- list-length start-position)
+                                         rev-selection)))
+                     (into [] 
+                           (concat (subvec start-state 0 start-position)
+                                   rev-selection
+                                   (subvec start-state idx-end list-length))))
+         new-start (if circled? 
+                     (mod (+ idx-end start-skip-size) list-length)
+                     (mod (+ start-position length start-skip-size) list-length))
+         ]
+    (KnotState. new-state new-start (inc start-skip-size))
     ))
+
+(def part-1-list (reduce #(make-move %1 %2) start-knot-state lengths-1))
+(def part-1 (apply * (take 2 (:list part-1-list))))
+
+(defn pad-input-list
+  [input-list]
+  (concat input-list default-end))
+
+(defn convert-to-ascii-char-code
+  [strng]
+  (map int strng))
+
+(defn run-hash-transform
+  [lengths start-state part-size]
+  (reduce #(make-move %1 %2)
+          start-state
+          (apply concat (repeat part-size lengths))))
+
+(defn dense-hash
+  [list part-size]
+  (let [split-list (partition part-size list)]
+    (map #(apply bit-xor %) split-list)))
+
+(defn tohex
+  [ints]
+  (apply str (map #(let [res (Integer/toString % 16)]
+                     (if (= 1 (count res))
+                       (str 0 res)
+                       res)) ints)))
+
+(defn knot-hash
+  ([strng]
+   (knot-hash (-> strng 
+                  string/trim
+                  convert-to-ascii-char-code 
+                  pad-input-list) 
+              start-knot-state 
+              rounds
+              partition-size))
+  ([lengths start-state rounds part-size]
+   (tohex 
+    (dense-hash 
+     (:list 
+      (run-hash-transform lengths start-state rounds)) 
+     part-size))))
+
+(def part-2 (knot-hash (slurp input-file)))
--- test/advent_of_code/day10-2017.clj
+++ test/advent_of_code/day10-2017.clj
@@ -7,8 +7,25 @@
 (def start-pos 0)
 (def start-skip-size 0)
 
-(def lengths '(3 4 1 5))
+(def lengths [3 4 1 5])
 (def start-state (KnotState. in-list start-pos start-skip-size))
 
 (deftest make-move-test
-  (is (= (KnotState. [2 1 0 3 4] 3 1) (day10/make-move start-state))))
+  (is (= (KnotState. [2 1 0 3 4] 3 1) (day10/make-move start-state (lengths 0))))
+  (is (= (KnotState. [4 3 0 1 2] 3 2) (day10/make-move (KnotState. [2 1 0 3 4] 3 1) (lengths 1))))
+  (is (= (KnotState. [4 3 0 1 2] 1 3) (day10/make-move (KnotState. [ 4 3 0 1 2] 3 2) (lengths 2))))
+  (is (= (KnotState. [3 4 2 1 0] 4 4) (day10/make-move (KnotState. [4 3 0 1 2] 1 3) (lengths 3)))))
+
+(deftest dense-hash-test
+  (is (= '(3 7) (day10/dense-hash [1 2 3 4] 2)))
+  )
+
+(deftest tohex-test
+  (is (= "4007ff" (day10/tohex [64 7 255]))))
+
+(deftest knot-hash-test
+  (is (= "a2582a3a0e66e6e86e3812dcb672a272" (day10/knot-hash "")))
+  (is (= "33efeb34ea91902bb2f59c9920caa6cd" (day10/knot-hash "AoC 2017")))
+  (is (= "3efbe78a8d82f29979031a4aa0b16a9d" (day10/knot-hash "1,2,3")))
+  (is (= "63960835bcdc130f0b66d7ff4f6a5a8e" (day10/knot-hash "1,2,4"))))
+
